be0692eb56641995f30472a668a6b9b6b78bc0b4
Josh <Josh@MININT-JJRLKIE.northamerica.corp.microsoft.com>
2020-12-12 08:43:54 -0500

    first commit

---
+++ tfidfpractice.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+documentA = 'the man went out for a walk'
+documentB = 'the children sat around the fire'
+
+bagOfWordsA = documentA.split(' ')
+bagOfWordsB = documentB.split(' ')
+
+print (type(bagOfWordsA))
